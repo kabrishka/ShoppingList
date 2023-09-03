c2ca25baabbef4da050524c420618e4e23345311
Karina <sharipovalliss@gmail.com>
2023-09-03 19:34:01 +0300

    add domain, data & presentation layers; create UseCases, repository and implemented a repo in the data layer

---
+++ app/src/main/java/com/kabrishka/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,56 @@
+package com.kabrishka.shoppinglist.data
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.kabrishka.shoppinglist.domain.ShopItem
+import com.kabrishka.shoppinglist.domain.ShopListRepository
+
+object ShopListRepositoryImpl : ShopListRepository {
+
+    private val shopListLD = MutableLiveData<List<ShopItem>>()
+
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementId = 0
+
+    init {
+        for (i in 0 until 10) {
+            val item = ShopItem("Name $i", i, true)
+            addShopItem(item)
+        }
+    }
+
+    override fun getShopList(): LiveData<List<ShopItem>> {
+        // используем копию shopList, чтобы не было возможности редактировать исходный список
+        return shopListLD
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find { it.id == shopItemId }
+            ?: throw RuntimeException("Element with $shopItemId not found")
+    }
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementId++
+        }
+        shopList.add(shopItem)
+        updateList()
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+        updateList()
+    }
+
+    private fun updateList() {
+        // value - можно вызвать только из главного потока
+        shopListLD.value = shopList.toList()
+    }
+}
